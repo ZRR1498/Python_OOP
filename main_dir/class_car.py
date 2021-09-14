bd3fc02e67201377bc31361ffc497010b83d4f63
ZRR1498 <zalyaliev14renat98off@gmail.com>
2021-09-14 21:35:06 +0300

    Вывод ссылки

--- main_dir/class_car.py
+++ main_dir/class_car.py
@@ -1,9 +1,14 @@
 class Car:
-
-    def __init__(self, model,color,year):
+    wheels = 4
+    def __init__(self, model,color,year, price):
         self.model = model
         self.color = color
         self.year = year
+        self.price = price
 
     def characters(self):
-        print("This car is " + self.model + "!")
+        print("Brand: " + self.model)
+        print("Color: " + self.color)
+        print("Year: " + self.year)
+        print("The cost of this car: " + self.price)
+
--- main_dir/main.py
+++ main_dir/main.py
@@ -1,5 +1,31 @@
 from class_car import Car
 
-car_1 = Car("Tesla", "Black", "2020")
+car_1 = Car("Tesla", "Black", "2021", "120000 $")
+car_2 = Car("Lada","White", "2010", "2000 $")
+car_3 = Car("Volvo","Black", "2016", "12000 $")
 
+print()
 car_1.characters()
+print("Number of wheels: ", car_1.wheels)
+print("-------------------------")
+car_2.characters()
+print("Number of wheels: ", car_2.wheels)
+
+def repit():
+    answer = 0
+    while answer != "YES" or "NO":
+        answer = input("Show dealers link? (Yes or No): ")
+        answer = answer.upper()
+        if answer == "YES" or "NO":
+            return answer
+
+answer = repit()
+if answer == "YES" or "NO":
+    if answer == "YES":
+        quest = input("Write brand of car: ")
+        quest = quest.lower()
+        answer = input("Show dealers link? (Yes or No): ")
+        on_display = ("https://www."+ str(quest) + ".com")
+
+    elif answer == "NO":
+        print("Good luck!")
